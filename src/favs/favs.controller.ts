ddbf57f97a9ca2e788107f8f162d5734743050b2
Azizbek Samatov <azizbeksamatov98@gmail.com>
2023-06-19 13:28:15 +0500

    fix: fixing issues with deleting in favorites controller and adding post endpoint

--- src/favs/favs.controller.ts
+++ src/favs/favs.controller.ts
@@ -7,4 +7,42 @@ import { FavsService } from './favs.service';
 @Controller('favs')
 export class FavsController {
   constructor(private readonly favsService: FavsService) {}
+
+  @Get()
+  findAll() {
+    return this.favsService.findAll();
+  }
+
+  @Post('track/:id')
+  addTrack(@Param('id', ParseUUIDPipe) id: string) {
+    return this.favsService.addItem('track', id);
+  }
+
+  @Delete('track/:id')
+  @HttpCode(204)
+  removeTrack(@Param('id', ParseUUIDPipe) id: string) {
+    return this.favsService.removeItem('track', id);
+  }
+
+  @Post('album/:id')
+  addAlbum(@Param('id', ParseUUIDPipe) id: string) {
+    return this.favsService.addItem('album', id);
+  }
+
+  @Delete('album/:id')
+  @HttpCode(204)
+  removeAlbum(@Param('id', ParseUUIDPipe) id: string) {
+    return this.favsService.removeItem('album', id);
+  }
+
+  @Post('artist/:id')
+  addArtist(@Param('id', ParseUUIDPipe) id: string) {
+    return this.favsService.addItem('artist', id);
+  }
+
+  @Delete('artist/:id')
+  @HttpCode(204)
+  removeArtist(@Param('id', ParseUUIDPipe) id: string) {
+    return this.favsService.removeItem('artist', id);
+  }
 }
